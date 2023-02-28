625607aa119d6fb0f8c214371f8ce4f4d810e0f2
Aaron <Aaron@DESKTOP-LN4JDIO>
2023-02-27 23:33:59 -0600

    tostr

--- Practica01/src/main/java/com/practica/service/PaisServiceImpl.java
+++ Practica01/src/main/java/com/practica/service/PaisServiceImpl.java
@@ -1,39 +1,48 @@
-package com.practica.service;
 
+package com.practica.service;
 import com.practica.dao.PaisDao;
 import com.practica.domain.Pais;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
 @Service
-public class PaisServiceImpl implements PaisService {
+public class PaisServiceImpl {
+   
 
     @Autowired
     PaisDao paisDao;
 
     @Override
+    public String toString() {
+        return "PaisServiceImpl{" + "paisDao=" + paisDao + '}';
+    }
+
+    
+   
+
+    
     @Transactional(readOnly = true)
     public List<Pais> getPaises() {
         return (List<Pais>) paisDao.findAll();
     }
 
-    @Override
+    
     @Transactional(readOnly = true)
     public Pais getPais(Pais pais) {
         return paisDao.findById(pais.getIdPais()).orElse(null);
     }
 
-    @Override
+    
     @Transactional
     public void saveCliente(Pais pais) {
         paisDao.save(pais);
     }
 
-    @Override
+    
     @Transactional
     public void deleteCliente(Pais pais) {
         paisDao.deleteById(pais.getIdPais());
     }
 }
+
