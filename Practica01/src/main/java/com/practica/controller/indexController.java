c290b8d5c7c61b89e80b9ec2351ad7938fd1cba3
Sofía Mora Muñoz <72779933+sofiamm@users.noreply.github.com>
2023-02-27 23:48:20 -0600

    add files

--- Practica01/src/main/java/com/practica/controller/indexController.java
+++ Practica01/src/main/java/com/practica/controller/indexController.java
@@ -1,24 +1,50 @@
 package com.practica.controller;
 
+import com.practica.domain.Pais;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import com.practica.service.PaisService;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Slf4j
 @Controller
 public class indexController {
+
     @Autowired
     PaisService paisService;
 
     @GetMapping("/")
     public String inicio(Model model) {
-        
+
         var paises = paisService.getPaises();
         model.addAttribute("listaPaises", paises);
-        
         return "index";
+    }
+    
+    @GetMapping("/nuevoPais")
+    public String nuevoCliente(Pais pais) {
+        return "modificarPais";
+    }
+
+    @PostMapping("/guardarPais")
+    public String guardarCliente(Pais pais) {
+        paisService.savePais(pais);
+        return "redirect:/";
+    }
+
+    @GetMapping("/modificarPais/{idPais}")
+    public String modificarCliente(Pais pais, Model model) {
+        pais = paisService.getPais(pais);
+        model.addAttribute("pais", pais);
+        return "modificarPais";
+    }
+
+    @GetMapping("/eliminarPais{idPais}")
+    public String eliminarCliente(Pais pais) {
+        paisService.deletePais(pais);
+        return "redirect:/";
     }
 }
--- Practica01/src/main/java/com/practica/service/PaisServiceImpl.java
+++ Practica01/src/main/java/com/practica/service/PaisServiceImpl.java
@@ -1,14 +1,14 @@
-
 package com.practica.service;
+
 import com.practica.dao.PaisDao;
 import com.practica.domain.Pais;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
 @Service
 public class PaisServiceImpl {
-   
 
     @Autowired
     PaisDao paisDao;
@@ -18,31 +18,23 @@ public class PaisServiceImpl {
         return "PaisServiceImpl{" + "paisDao=" + paisDao + '}';
     }
 
-    
-   
-
-    
     @Transactional(readOnly = true)
     public List<Pais> getPaises() {
         return (List<Pais>) paisDao.findAll();
     }
 
-    
     @Transactional(readOnly = true)
     public Pais getPais(Pais pais) {
         return paisDao.findById(pais.getIdPais()).orElse(null);
     }
 
-    
     @Transactional
     public void saveCliente(Pais pais) {
         paisDao.save(pais);
     }
 
-    
     @Transactional
     public void deleteCliente(Pais pais) {
         paisDao.deleteById(pais.getIdPais());
     }
 }
-
